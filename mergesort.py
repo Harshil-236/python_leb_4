804980a801347ad098c8bacba276c90d16fb7084
Harshil-236 <bca2022harshil1780@tnraocollege.org>
2024-08-08 17:36:35 +0530

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -35,3 +35,5 @@ sorted_arr = merge_sort(arr)
 print("Sorted Array:", sorted_arr)
 
 output:-
+![Screenshot 2024-08-08 171900](https://github.com/user-attachments/assets/051ab64e-c88f-4343-871b-bd5dba63a1ae)
+
